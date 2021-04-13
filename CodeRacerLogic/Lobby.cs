e2f10d3b2d0e531018d0222a4362b2a66b76b6e4
ctorD <danclarke98@gmail.com>
2021-04-13 23:40:00 +0100

    Upgraded to .Net 5

--- CodeRacerLogic/Lobby.cs
+++ CodeRacerLogic/Lobby.cs
@@ -5,75 +5,76 @@ using System.Diagnostics;
 
 namespace CodeRacerBackend.CodeRacerLogic
 {
-    public class Lobby
+  public class Lobby
+  {
+    public string LobbyId { get; set; }
+
+    public string LobbyName { get; set; }
+
+    public string Host { get; set; }
+    public int MaxPlayers { get; set; }
+    public string Snippet { get; set; }
+    public List<String> Players { get; set; }
+
+    public List<Tuple<String, TimeSpan>> Scores = new List<Tuple<string, TimeSpan>>();
+    private Stopwatch stopWatch = new Stopwatch();
+
+    public string GenerateLobbyId(string user)
     {
-        public string LobbyId { get; set; }
-
-        public string LobbyName { get; set; }
-
-        public string Host { get; set; }
-        public int MaxPlayers { get; set; }
-        public string Snippet { get; set; }
-        public List<String> Players { get; set; }
-
-        public List<Tuple<String, TimeSpan>> Scores = new List<Tuple<string, TimeSpan>>();
-        private Stopwatch stopWatch = new Stopwatch();
-
-        public string GenerateLobbyId(string user)
-        {
-            return string.Format("{0}_{1:N}", user, Guid.NewGuid());
-        }
-
-        public Lobby(string lang, string user, string lobbyName, bool online)
-        {
-            LobbyName = lobbyName;
-            Host = user;
-            this.MaxPlayers = online ? 4 : 1;
-            this.LobbyId = GenerateLobbyId(user);
-            SnippetFinder sf = new SnippetFinder();
-            this.Snippet = sf.getSnippet(lang);
-            this.Players = new List<string>();
-        }
-
-        public Boolean Join(string connectionId)
-        {
-            if (!Players.Contains(connectionId) && Players.Count != MaxPlayers)
-            {
-                Players.Add(connectionId);
-                return true;
-            }
-            else
-            {
-                return false;
-            }
-        }
-
-        public void Leave(string connectionId)
-        {
-            Players.Remove(connectionId);
-        }
-
-        public void Start()
-        {
-            stopWatch.Start();
-            if (stopWatch.IsRunning)
-            {
-                Console.WriteLine(stopWatch.Elapsed.TotalMilliseconds);
-            }
-            Console.WriteLine("Timer Starting for" + LobbyId);
-        }
-
-        public void GameFinished(string connectionId, string time)
-        {
-        }
-
-        public void PlayerComplete(string user, string time)
-        {
-            Scores.Add(new Tuple<String, TimeSpan>(user, stopWatch.Elapsed));
-            if (Scores.Count != MaxPlayers)
-            {
-                Scores.Add(new Tuple<String, TimeSpan>(user, TimeSpan.Parse(time)));
-            }
-        }
+      return string.Format("{0}_{1:N}", user, Guid.NewGuid());
     }
+
+    public Lobby(string lang, string user, string lobbyName, bool online)
+    {
+      LobbyName = lobbyName;
+      Host = user;
+      this.MaxPlayers = online ? 4 : 1;
+      this.LobbyId = GenerateLobbyId(user);
+      SnippetFinder sf = new SnippetFinder();
+      this.Snippet = sf.getSnippet(lang);
+      this.Players = new List<string>();
+    }
+
+    public Boolean Join(string connectionId)
+    {
+      if (!Players.Contains(connectionId) && Players.Count != MaxPlayers)
+      {
+        Players.Add(connectionId);
+        return true;
+      }
+      else
+      {
+        return false;
+      }
+    }
+
+    public void Leave(string connectionId)
+    {
+      Players.Remove(connectionId);
+    }
+
+    public void Start()
+    {
+      stopWatch.Start();
+      if (stopWatch.IsRunning)
+      {
+        Console.WriteLine(stopWatch.Elapsed.TotalMilliseconds);
+      }
+      Console.WriteLine("Timer Starting for" + LobbyId);
+    }
+
+    public void GameFinished(string connectionId, string time)
+    {
+    }
+
+    public void PlayerComplete(string user, string time)
+    {
+      // server Time, use to compare.
+      // Scores.Add(new Tuple<String, TimeSpan>(user + "stopwatch", stopWatch.Elapsed));
+      if (Scores.Count != MaxPlayers)
+      {
+        Scores.Add(new Tuple<String, TimeSpan>(user + "timespan", TimeSpan.Parse(time)));
+      }
+    }
+  }
 }
